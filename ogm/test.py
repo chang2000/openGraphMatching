6639353603acd8ba0fde45328b918baed8529480
Alex WANG <wangtcalex@gmail.com>
2020-09-08 22:20:53 +0800

    update

--- ogm/test.py
+++ ogm/test.py
@@ -1,11 +1,29 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+from SubGraphMatcher import SubGraphMatcher
 
-G = nx.Graph()
-G.add_nodes_from([1, 2, 3, 4, 5, 6])
-G.add_edges_from([(1, 2), (3, 4), (1, 3), (3, 5), (5, 6)])
-print(G.nodes)
-print(G.edges)
+def draw_graph(G):
+    options = {
+        'node_color': 'yellow',
+        'node_size': 300,
+        'width': 3,
+        'with_labels': True
+    }
+    nx.draw(G, **options)
+    plt.show()
 
-nx.draw(G, with_labels=True)
-plt.show()
+# G_t = nx.path_graph(4000)
+# G_t = nx.complete_graph(10)
+G_t = nx.path_graph(10)
+G_q = nx.path_graph(5)
+
+# draw_graph(G_q)
+# draw_graph(G_t)
+
+SGM = SubGraphMatcher(G_q, G_t)
+print(SGM.is_subgraph_match())
+
+# G_q.add_node(3)
+# SGM = SubGraphMatcher(G_q, G_t)
+# print(SGM.is_subgraph_match())
+
