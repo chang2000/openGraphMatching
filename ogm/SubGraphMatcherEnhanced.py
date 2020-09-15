8e92ff1dfca4b77b3f1161b8310ccab1a5b9eaf0
Alex WANG <wangtcalex@gmail.com>
2020-09-15 16:01:20 +0800

    need to do the edge check

--- ogm/SubGraphMatcherEnhanced.py
+++ ogm/SubGraphMatcherEnhanced.py
@@ -1,77 +1,57 @@
 import networkx as nx
 from itertools import combinations
 import matplotlib.pyplot as plt
+import sys
 
 class SubGraphMatcher:
-    def __init__(self, G_t):
+    def __init__(self, G):
         """
         G_q: the query graph
-        G_t: the target graph
+        G: the target graph
 
         Examples:
         -------
         >>> import SubGraphMatcher
-        >>> G_t = nx.path_graph(4)
+        >>> G = nx.path_graph(4)
         >>> G_q = nx.path_graph(2)
-        >>> SGM = SubGraphMatcher(G_t)
+        >>> SGM = SubGraphMatcher(G)
         >>> SGM.is_subgraph_match(G_q)
             True
         """
         try:
-            assert (nx.is_connected(G_t))
+            assert (nx.is_connected(G))
         except:
             print('Input graphs should be connected')
             exit()
-        self.G_t = G_t
-        self.G_t_nodes = list(G_t.nodes())
+        self.G = G
+        self.G_nodes = list(G.nodes())
         self.check_result = None
+        self.M = []
 
     def generate_all_subgraphs(self, G_q):
-        combs = list(combinations(self.G_t_nodes, len(list(G_q.nodes))))
+        combs = list(combinations(self.G_nodes, len(list(G_q.nodes))))
         res = []
         for i in range(len(combs)):
-            graph = self.G_t.subgraph(list(combs[i]))
+            graph = self.G.subgraph(list(combs[i]))
             if nx.is_connected(graph): # Only generate connected candidates
                 res.append([combs[i], graph])
         return res
 
-    def check_match_subgraph(self, G_q):
-        # Type Check, the input G_q must be a networkx instance and connected
-        try:
-            assert (isinstance(G_q, nx.classes.graph.Graph) and nx.is_connected(G_q))
-        except:
-            print('Input query graph must be a single networkx instance.')
-            exit()
-
-        candidates = self.generate_all_subgraphs(G_q)
-        for candidate in candidates:
-            if nx.is_isomorphic(candidate[1], G_q):
-                self.check_result = candidate[0]
-                self.draw_check_result()
-                print('Matched!', candidate[0])
-                return True
-        return False
 
     def draw_check_result(self):
         if self.check_result != None:
-            labels = nx.get_node_attributes(self.G_t, 'feat')
-            pos = nx.spring_layout(self.G_t)
-            options = {
-                        # 'node_color': 'yellow',
-                        'node_size': 400,
-                        # 'width': 3,
-                        # 'labels': labels,
-                        # 'with_labels': True
-                        }
-            subgraph = self.G_t.subgraph(list(self.check_result))
+            labels = nx.get_node_attributes(self.G, 'feat')
+            pos = nx.spring_layout(self.G)
+            options = { 'node_size': 400 }
+            subgraph = self.G.subgraph(list(self.check_result))
             nx.draw_networkx_nodes(
-                    self.G_t, 
+                    self.G, 
                     pos, 
-                    nodelist=list(self.G_t.nodes()), 
+                    nodelist=list(self.G.nodes()), 
                     node_color='yellow', 
                     **options)
             # Draw the subgraph nodes
-            nx.draw_networkx_nodes(self.G_t, 
+            nx.draw_networkx_nodes(self.G, 
                     pos, 
                     nodelist=list(subgraph.nodes()), 
                     node_color='red', 
@@ -79,20 +59,20 @@ class SubGraphMatcher:
 
             # Draw all the edges
             nx.draw_networkx_edges(
-                    self.G_t, 
+                    self.G, 
                     pos, 
-                    edgelist=list(self.G_t.edges()), 
+                    edgelist=list(self.G.edges()), 
                     width=3, 
                     edge_color='black')
             # Draw the subgraph edges
             nx.draw_networkx_edges(
-                    self.G_t, 
+                    self.G, 
                     pos, 
                     edgelist=list(subgraph.edges()), 
                     width=3, 
                     edge_color='red')
-            labels = nx.get_node_attributes(self.G_t, 'feat') 
-            nx.draw_networkx_labels(self.G_t, pos, labels, font_size=16)
+            labels = nx.get_node_attributes(self.G, 'feat') 
+            nx.draw_networkx_labels(self.G, pos, labels, font_size=16)
             plt.show()
 
     # 如何复用计算方式
@@ -128,7 +108,6 @@ class SubGraphMatcher:
         return res
 
     def NLF(self, q, G, candidates):
-    # def NLF(self, q, G):
         """
         This function takes in a query graph and a target graph
 
@@ -165,21 +144,106 @@ class SubGraphMatcher:
                     # Compute N(u, l)
                     q_feats = [q.nodes[n]['feat'] for n in u_neighbors]
                     nul = q_feats.count(l)
-                    print('nul', nul)
-                    # compute N(v, l)
+                    # Compute N(v, l)
                     G_feats = [G.nodes[n]['feat'] for n in v_neighbors]
                     nvl = G_feats.count(l)
-                    print('nvl', nvl)
                     if nul > nvl:
                         # remove v from candidate
                         # print(v)
                         # print('candidate is', candidates)
-                        candidates = [c for c in candidates if c[1] != v]
+                        candidates = [c for c in candidates if not (c[0] == u and c[1] == v)]
                         # print('candidates after filtering', candidates)
+        print(candidates)
         return candidates
 
-        
-     
-        
+    # Ordering Parts
+    def gen_ordering_order(self, q):
+        return list(q.nodes())
+
+    # Enumeration Methods
+    def enumerate(self, q, G, C, A, order, i):
+        if i == len(order) + 1:
+            print('matching output:', self.M)
+            return self.M
+
+        # v is a extenable vertex
+        u = self.get_extenable_vertex(order)
+        lc = self.computeLC(q, G, C, A, order, u, i)
+        print('lc is', lc)
+        for v in lc:
+            print(v)
+            if v not in self.M:
+                self.M.append(v)
+                self.enumerate(q, G, C, A, order, i + 1)
+                self.M.pop()
+
+    # ComputeLC of QuickSI and RI
+    def computeLC(self, q, G, C, A, order, u, i):
+        # print(C)
+        # print(u)
+        if i == 1: # do not care the edge
+            return [c for c in C if c[0] == u]
+        else: # examine the edge
+            # get the parent of current match
+            length = len(self.M)
+            parent_of_u = self.M[length - 2][0]
+            M_u_p = self.M[length - 2][1]
+            print(M_u_p)
+            # for v in G.neighbors()
+
+            # Default
+            return [c for c in C if c[0] == u]
 
 
+
+    def get_extenable_vertex(self, order):
+        # order is just list on index here
+        # I want to know what's the matching going on
+        # find the latest match (right or wrong)
+        length = len(self.M)
+        print(self.M)
+        if length == 0:
+            return order[0]
+        else:
+            latest_node = self.M[length - 1] 
+            print(latest_node)
+            index = order.index(latest_node[0])
+            print('return order is', index + 1 )
+            return order[index + 1]
+  
+    """
+    Rethink: 
+    1. M should be a dict to make it easy to operate
+
+
+    """
+    def check_match_subgraph (self, q):
+        try:
+            assert (isinstance(q, nx.classes.graph.Graph) and nx.is_connected(q))
+        except:
+            print('Input query graph must be a single networkx instance.')
+            sys.exit()
+
+        C = self.NLF(q, self.G, self.LDF(q, self.G))
+        A = None
+        order = self.gen_ordering_order(q)
+        res = self.enumerate(q, self.G, C, A, order, 1)
+        print(res)
+        
+
+    # The naive way
+    # def check_match_subgraph(self, G_q):
+        # try:
+            # assert (isinstance(G_q, nx.classes.graph.Graph) and nx.is_connected(G_q))
+        # except:
+            # print('Input query graph must be a single networkx instance.')
+            # exit()
+
+        # candidates = self.generate_all_subgraphs(G_q)
+        # for candidate in candidates:
+            # if nx.is_isomorphic(candidate[1], G_q):
+                # self.check_result = candidate[0]
+                # self.draw_check_result()
+                # print('Matched!', candidate[0])
+                # return True
+        # return False
