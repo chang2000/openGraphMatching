0d6de7bede0951d2e39c0c71d96f1856ed64b993
Alex WANG <wangtcalex@gmail.com>
2020-09-21 19:28:51 +0800

    update

--- ogm/multi-match.py
+++ ogm/multi-match.py
@@ -7,19 +7,18 @@ import time
 
 master_time = time.time()
 
-# G = convert_graph('./dataset/data_graph/HPRD.graph')
-G = convert_graph('./dataset/sample_dataset_copy/target.graph')
+G = convert_graph('./dataset/data_graph/HPRD.graph')
+# G = convert_graph('./dataset/sample_dataset_copy/target.graph')
 print(nx.is_connected(G))
 SGM = SubGraphMatcher(G)
-dataset_path = './dataset/sample_dataset_copy/'
-# dataset_path = './dataset/'
+dataset_path = './dataset/'
+# dataset_path = './dataset/sample_dataset_copy/'
 queries = os.listdir(dataset_path + 'query_graph')
 
 counter = 0
 for e in queries:
     counter += 1
     q = convert_graph(dataset_path + 'query_graph/' + e)
-    # q = convert_graph('./dataset/query_graph/' + e)
     print(f'Running query {counter}...')
     SGM.check_match_subgraph(q)
 
