54b3db8d003f84f34d9d850017d0969c7f3b155c
Alex WANG <wangtcalex@gmail.com>
2020-09-21 19:17:20 +0800

    complete multi integration

--- ogm/multi-match.py
+++ ogm/multi-match.py
@@ -3,8 +3,9 @@ from SubGraphMatcherEnhanced import SubGraphMatcher
 from utils.convert_graph import convert_graph
 import sys
 import os
-# get the file list
+import time
 
+master_time = time.time()
 
 # G = convert_graph('./dataset/data_graph/HPRD.graph')
 G = convert_graph('./dataset/sample_dataset_copy/target.graph')
@@ -12,8 +13,8 @@ print(nx.is_connected(G))
 SGM = SubGraphMatcher(G)
 dataset_path = './dataset/sample_dataset_copy/'
 # dataset_path = './dataset/'
-# queries = os.listdir(dataset_path + 'query_graph')
 queries = os.listdir(dataset_path + 'query_graph')
+
 counter = 0
 for e in queries:
     counter += 1
@@ -22,5 +23,7 @@ for e in queries:
     print(f'Running query {counter}...')
     SGM.check_match_subgraph(q)
 
+print("All queries done, average time is")
+print(f'--- {(time.time() - master_time) / len(queries)} ---')
 
 
