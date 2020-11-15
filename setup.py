41afa1222ad0f85292bc2747e4b905a6c2131cfc
Alex WANG <alex@pn-204-28.itsc.cuhk.edu.hk>
2020-11-15 11:10:41 +0800

    update version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="openGraphMatching",
-    version="0.0.9",
+    version="0.1.0",
     author="",
     author_email="",
     description="A subgraph matching programming framework.",
