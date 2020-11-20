13f398becb13ea0e0f202478867c607d3239b5df
WANG Tianchang Alex <wangtcalex@gmail.com>
2020-11-20 12:30:32 +0800

    update the project structure, the exact matcher framework done

---
+++ examples/exact_subgraph_match_validate.py
@@ -0,0 +1,27 @@
+import os.path as osp
+import networkx as nx
+import sys
+import random
+from openGraphMatching.utils import convert_graph, check_match_correctness, draw_graph
+from openGraphMatching.matcher import GQLMatcher, CECIMatcher
+
+dataset = 'validate'
+path = osp.join(osp.dirname(osp.realpath(__file__)), '../dataset', dataset) 
+datasetpath = path + '/query_graph/'
+G = convert_graph(path + '/data_graph/HPRD.graph')
+
+of = open('generated_ans.res', 'w')
+matcher = GQLMatcher(G)
+
+query_prefix = 'query_dense_16_'
+# sample = random.sample(range(1,201), 20) 
+# print(sample)
+# for i in sample:
+for i in range(1, 201):
+    query_name = query_prefix + str(i)
+    query_path = query_name + '.graph'
+    print(query_path)
+    q = convert_graph(datasetpath + query_path)
+    data = matcher.is_subgraph_match(q)
+    of.write(f'{query_path}:{len(data[1])}\n')
+
---
+++ examples/generated_ans.res
@@ -0,0 +1,1 @@
+query_dense_16_1.graph:3
